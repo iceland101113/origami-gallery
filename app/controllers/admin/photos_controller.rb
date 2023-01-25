f74348921fa2be3aa2013b714bfe3c42e1859bda
iceland101113 <iceland101113@gmail.com>
2023-01-25 17:16:16 +0800

    update admin page per page items

--- app/controllers/admin/photos_controller.rb
+++ app/controllers/admin/photos_controller.rb
@@ -2,7 +2,7 @@ class Admin::PhotosController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    @small_items = SmallItem.page(params[:page]).per(3)
-    @big_items = BigItem.page(params[:page]).per(5)
+    @small_items = SmallItem.page(params[:page]).per(10)
+    @big_items = BigItem.page(params[:page]).per(10)
   end
 end
