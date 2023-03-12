dc7b1a506e3814a1a2c54c68719c121dfd00428c
iceland101113 <iceland101113@gmail.com>
2023-03-12 18:29:08 +0800

    layout adjustment

--- app/controllers/admin/photos_controller.rb
+++ app/controllers/admin/photos_controller.rb
@@ -2,7 +2,7 @@ class Admin::PhotosController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    @small_items = SmallItem.page(params[:page]).per(10)
-    @big_items = BigItem.page(params[:page]).per(10)
+    @small_items = SmallItem.order(updated_at: :desc).page(params[:page]).per(10)
+    @big_items = BigItem.order(updated_at: :desc).page(params[:page]).per(10)
   end
 end
